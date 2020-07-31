210684ac4f22504feff7437e428bd2083c254da2
Sibylau <jl3952@cornell.edu>
2020-07-31 16:55:29 -0400

    initialize

---
+++ experiments/helloworld/hello.s
@@ -0,0 +1,28 @@
+	.text
+	.file	"hello.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entry
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movl	$.Lstr, %edi
+	callq	puts
+	xorl	%eax, %eax
+	popq	%rcx
+	.cfi_def_cfa_offset 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.Lstr,@object                   # @str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lstr:
+	.asciz	"hello world"
+	.size	.Lstr, 12
+
+	.ident	"clang version 12.0.0 (https://github.com/llvm/llvm-project.git 724bf4ee23a3993689d55afb2845949e05c83b1c)"
+	.section	".note.GNU-stack","",@progbits
